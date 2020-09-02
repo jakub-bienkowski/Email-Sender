5b4def7c3f3738dcfb94d0e34c53315296d32d13
Jakub Bieńkowski <jbienkowski94@gmail.com>
2020-09-02 20:30:59 +0200

    added styling

---
+++ src/main/java/com/jb/MailService/Service.java
@@ -0,0 +1,45 @@
+package com.jb.MailService;
+
+import javax.mail.*;
+import javax.mail.internet.*;
+import java.util.Properties;
+
+public class Service {
+
+Properties prop = new Properties();
+
+    public void loadProperties() {
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.ssl.enable", "true");
+        prop.put("mail.smtp.starttls.enable", "false");
+        prop.put("mail.smtp.host", "smtp.poczta.onet.pl");
+        prop.put("mail.smtp.port", "465");
+        prop.put("mail.smtp.ssl.trust", "smtp.poczta.onet.pl");
+    }
+
+    Session session = Session.getInstance(prop, new Authenticator() {
+
+        @Override
+        protected PasswordAuthentication getPasswordAuthentication() {
+            return new PasswordAuthentication("jakub.bienkowski94@onet.pl", "infoShare1");
+        }
+    });
+
+    public void send(String email, String subject, String userMessage) throws MessagingException {
+        session.setDebug(true);
+        Message message = new MimeMessage(session);
+        message.setFrom(new InternetAddress("jakub.bienkowski94@onet.pl"));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+        message.setSubject(subject);
+
+        MimeBodyPart mimeBodyPart = new MimeBodyPart();
+        mimeBodyPart.setContent(userMessage, "text/html");
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(mimeBodyPart);
+
+        message.setContent(multipart);
+        Transport.send(message);
+    }
+
+}
--- src/main/java/com/jb/servlets/MainServlet.java
+++ src/main/java/com/jb/servlets/MainServlet.java
@@ -1,12 +1,10 @@
 package com.jb.servlets;
 
-import com.jb.freemarker.TemplateProvider;
-import freemarker.template.Template;
-import freemarker.template.TemplateException;
+import com.jb.MailService.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
+import javax.mail.MessagingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,17 +12,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 @WebServlet("/mail")
 public class MainServlet extends HttpServlet {
 
     private static final Logger STDOUT = LoggerFactory.getLogger("CONSOLE_OUT");
-    private static final String TEMPLATE_NAME = "index.ftml";
-
-    @Inject
-    private TemplateProvider templateProvider;
 
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setCharacterEncoding("UTF-8");
@@ -33,25 +25,17 @@ public class MainServlet extends HttpServlet {
         String subject = req.getParameter("subject");
         String message = req.getParameter("message");
 
+        Service service = new Service();
+        service.loadProperties();
+        try {
+            service.send(email, subject, message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
         PrintWriter writer = resp.getWriter();
-        writer.println("<html>Your email: " + email + " subject: " + subject + "message: " + message +
-                "</html>");
+        writer.println("Your e-mail was sent successfully");
 
     }
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-//        resp.setCharacterEncoding("UTF-8");
-//
-//        Map<String, Object> map = new HashMap<>();
-//        String welcome = "Welcome!";
-//        map.put("hello", welcome);
-//
-//        Template template = templateProvider.createTemplate(getServletContext(), TEMPLATE_NAME);
-//        try {
-//            template.process(map, resp.getWriter());
-//        } catch (TemplateException e) {
-//            STDOUT.error("Error while processing template: ", e);
-//        }
-    }
 }
